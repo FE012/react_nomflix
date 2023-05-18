3d900387223765707e8ce330f777320d10d33224
FE012 <twte96@gmail.com>
2023-05-18 22:17:09 +0900

    chore: nomflix 프로젝트 초기세팅

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route></Route>
+        <Route></Route>
+        <Route></Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
