bf1612cd35cb8e5932f326c909f77686c1491a73
FE012 <twte96@gmail.com>
2023-05-19 09:44:04 +0900

    chore:9.1 Header part 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Routes/Home";
+import Search from "./Routes/Search";
+import Tv from "./Routes/Tv";
 
 function App() {
   return (
     <Router>
+      <Header />
       <Switch>
-        <Route></Route>
-        <Route></Route>
-        <Route></Route>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/tv">
+          <Tv />
+        </Route>
+        <Route path="/search">
+          <Search />
+        </Route>
       </Switch>
     </Router>
   );
