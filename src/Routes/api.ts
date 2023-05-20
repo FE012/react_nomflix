95901e068b2b4bf3f3e17f3e8623ae27ab0102f7
FE012 <twte96@gmail.com>
2023-05-20 17:32:42 +0900

    chore: 9.6 Home Screen part 2

--- src/Routes/api.ts
+++ src/Routes/api.ts
@@ -1,6 +1,24 @@
 const API_KEY = "30b9835919252c28ccb9f37e2f04774e";
 const BASE_PATH = "https://api.themoviedb.org/3";
 
+interface IMovie {
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  overview: string;
+}
+
+export interface IGetMoviesResult {
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: IMovie[];
+  total_pages: number;
+  total_results: number;
+}
+
 export function getMovies() {
   return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
     (response) => response.json()
