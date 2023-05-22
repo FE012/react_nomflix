32f083ae891f24adbada4214e76a60871daa5dd6
FE012 <twte96@gmail.com>
2023-05-23 00:14:38 +0900

    영화 slider 추가

--- src/Routes/api.ts
+++ src/Routes/api.ts
@@ -1,6 +1,7 @@
 const API_KEY = "30b9835919252c28ccb9f37e2f04774e";
 const BASE_PATH = "https://api.themoviedb.org/3";
 
+//common result
 interface IMovie {
   backdrop_path: string;
   poster_path: string;
@@ -9,6 +10,7 @@ interface IMovie {
   id: number;
 }
 
+//Latest movies
 export interface IGetMoviesResult {
   dates: {
     maximum: string;
@@ -20,8 +22,24 @@ export interface IGetMoviesResult {
   total_results: number;
 }
 
+//Top Rated Movies
+export interface IGetTopMoviesResult {
+  page: number;
+  results: IMovie[];
+  total_pages: number;
+  total_results: number;
+}
+
+//Latest movies
 export function getMovies() {
   return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
+    (response) => response.json()
+  );
+}
+
+//Top Rated Movies
+export function getTopMovies() {
+  return fetch(`${BASE_PATH}/movie/top_rated?api_key=${API_KEY}`).then(
     (response) => response.json()
   );
 }
