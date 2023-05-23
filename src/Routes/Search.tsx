9db106c873704de27fc6413a4c75cbf1d9d0d7b4
FE012 <twte96@gmail.com>
2023-05-24 02:49:01 +0900

    chore: 검색결과 안내문 구현, keyword prop 보내기

--- src/Routes/Search.tsx
+++ src/Routes/Search.tsx
@@ -1,9 +1,35 @@
 import { useLocation } from "react-router-dom";
+import styled from "styled-components";
+import SearchResult from "./SearchResult";
+
+const Wrapper = styled.div`
+  padding: 130px 55px 0px 55px;
+`;
+
+const TopWrap = styled.div`
+  margin-bottom: 20px;
+`;
+
+const Notice = styled.p`
+  font-size: 25px;
+  font-weight: 400;
+`;
+
+const BottomWrap = styled.div``;
 
 function Search() {
   const location = useLocation();
-  console.log(location);
-  return null;
+  const keyword = new URLSearchParams(location.search).get("keyword");
+  console.log(keyword);
+
+  return (
+    <Wrapper>
+      <TopWrap>
+        <Notice>'{keyword}'(으)로 검색한 결과</Notice>
+      </TopWrap>
+      <BottomWrap>{keyword && <SearchResult keyword={keyword} />}</BottomWrap>
+    </Wrapper>
+  );
 }
 
 export default Search;
