9c19d403c628ef045a6106b83cba5410804b41ba
FE012 <twte96@gmail.com>
2023-05-31 16:06:36 +0900

    chore: 검색결과 없을때 이미지 불러오기

--- src/Routes/util.ts
+++ src/Routes/util.ts
@@ -1,3 +1,10 @@
+// import noSearchIcon from "../assets/noSearchIcon.png";
+const noSearchIcon = require("../assets/noSearchIcon.png");
+
 export function makeImagePath(id: string, format?: string) {
   return `https://image.tmdb.org/t/p/${format ? format : "original"}/${id}`;
+}
+
+export function noSearchDataImage() {
+  return noSearchIcon;
 }
