a27069b0b1e35bcd45acca291d35616fc6b79024
FE012 <twte96@gmail.com>
2023-06-06 16:03:42 +0900

    chore: api 언어변경 및 모달창 api 추가

--- src/Routes/api.ts
+++ src/Routes/api.ts
@@ -1,5 +1,16 @@
 const API_KEY = "30b9835919252c28ccb9f37e2f04774e";
 const BASE_PATH = "https://api.themoviedb.org/3";
+const LANGUAGE = "ko-KO";
+const REGION = "KR";
+const TAIL_PATH = `api_key=${API_KEY}&language=${LANGUAGE}&region=${REGION}`;
+export const LIST_TYPE = [
+  "latestMovies",
+  "topRatedMovies",
+  "upcomingMovies",
+  "airingToday",
+  "topRated",
+  "popular",
+]; //영상 종류
 
 //common result interface
 interface IMovie {
@@ -9,6 +20,8 @@ interface IMovie {
   overview: string;
   id: number;
   vote_average: number;
+  original_title: string;
+  release_date: string;
 }
 
 //Latest movies interface
@@ -41,22 +54,22 @@ export interface IGetTopMoviesResult {
 
 //Latest Movies
 export function getMovies() {
-  return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
-    (response) => response.json()
+  return fetch(`${BASE_PATH}/movie/now_playing?${TAIL_PATH}`).then((response) =>
+    response.json()
   );
 }
 
 //Top Rated Movies
 export function getTopMovies() {
-  return fetch(`${BASE_PATH}/movie/top_rated?api_key=${API_KEY}`).then(
-    (response) => response.json()
+  return fetch(`${BASE_PATH}/movie/top_rated?${TAIL_PATH}`).then((response) =>
+    response.json()
   );
 }
 
 //Upcoming Movies
 export function getUpcomingMovies() {
-  return fetch(`${BASE_PATH}/movie/upcoming?api_key=${API_KEY}`).then(
-    (response) => response.json()
+  return fetch(`${BASE_PATH}/movie/upcoming?${TAIL_PATH}`).then((response) =>
+    response.json()
   );
 }
 
@@ -78,37 +91,37 @@ export interface IGetTopTvResult {
   total_results: number;
 }
 
-//Latest Shows TV
-// export function getLatestTv() {
-//   return fetch(`${BASE_PATH}/tv/latest?api_key=${API_KEY}`).then((response) =>
-//     response.json()
-//   );
-// }
-
 //Airing Today TV
 export function getAiringTv() {
-  return fetch(`${BASE_PATH}/tv/airing_today?api_key=${API_KEY}`).then(
-    (response) => response.json()
+  return fetch(`${BASE_PATH}/tv/airing_today?${TAIL_PATH}`).then((response) =>
+    response.json()
   );
 }
 
 //Popular Tv
 export function getPopularTv() {
-  return fetch(`${BASE_PATH}/tv/popular?api_key=${API_KEY}`).then((response) =>
+  return fetch(`${BASE_PATH}/tv/popular?${TAIL_PATH}`).then((response) =>
     response.json()
   );
 }
 
 //Top Rated Tv
 export function getTopRatedTv() {
-  return fetch(`${BASE_PATH}/tv/top_rated?api_key=${API_KEY}`).then(
-    (response) => response.json()
+  return fetch(`${BASE_PATH}/tv/top_rated?${TAIL_PATH}`).then((response) =>
+    response.json()
   );
 }
 
 //Search Movie and TV
 export function getSearch(keyword: string) {
-  return fetch(
-    `${BASE_PATH}/search/multi?api_key=${API_KEY}&query=${keyword}`
-  ).then((response) => response.json());
+  return fetch(`${BASE_PATH}/search/multi?${TAIL_PATH}&query=${keyword}`).then(
+    (response) => response.json()
+  );
+}
+
+//Modal getDetail Info
+export function getDetailData(mediaType: string, dataId: string) {
+  return fetch(`${BASE_PATH}/${mediaType}/${dataId}?${TAIL_PATH}`).then(
+    (response) => response.json()
+  );
 }
