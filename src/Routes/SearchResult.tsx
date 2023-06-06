87739df12d7d2e4201541997aeee73e26948bffd
FE012 <twte96@gmail.com>
2023-06-06 16:01:12 +0900

    feat: 검색결과에서 클릭시 모달창 구현

--- src/Routes/SearchResult.tsx
+++ src/Routes/SearchResult.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
-import { motion } from "framer-motion";
-import { useHistory } from "react-router-dom";
+import { AnimatePresence, motion, useScroll } from "framer-motion";
+import { useHistory, useRouteMatch } from "react-router-dom";
 import styled from "styled-components";
 import { getSearch } from "./api";
 import { makeImagePath, noSearchDataImage } from "./util";
@@ -80,36 +80,137 @@ const infoVariants = {
   },
 };
 
+//모달창 스타일 컴포넌트
+const Overlay = styled(motion.div)`
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  opacity: 0;
+`;
+
+const BigMovie = styled(motion.div)`
+  position: absolute;
+  width: 40vw;
+  height: 80vh;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+  overflow: hidden;
+  border-radius: 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background-color: ${(props) => props.theme.black.lighter};
+  z-index: 99;
+`;
+
+const BigCover = styled.div`
+  width: 100%;
+  background-size: cover;
+  background-position: center center;
+  height: 350px;
+`;
+
+const BigTitle = styled.h3`
+  color: ${(props) => props.theme.white.lighter};
+  padding: 10px;
+  font-size: 36px;
+  position: relative;
+  top: -60px;
+`;
+
+const BigOverview = styled.p`
+  padding: 20px;
+  position: relative;
+  top: -80px;
+  color: ${(props) => props.theme.white.lighter};
+`;
+
+const BigAverage = styled.h4`
+  font-size: 22px;
+  margin: 10px;
+`;
+
 function SearchResult({ keyword }: { keyword: string }) {
-  const { isLoading, data: search } = useQuery(["search", keyword], () =>
+  const { data: search } = useQuery(["search", keyword], () =>
     getSearch(keyword)
   );
-
   console.log(search);
-
+  const { scrollY } = useScroll();
+  const bigMatch = useRouteMatch<{ id: string }>("/search/:media/:id");
+  console.log(bigMatch);
   const history = useHistory();
-  const onBoxClicked = (id: number) => {};
+  const onBoxClicked = (media: string, id: number) => {
+    history.push(`/search/${media}/${id}?keyword=${keyword}`);
+  };
+  const onOverlayClick = () => history.goBack();
+
+  const clickedBox =
+    bigMatch?.params.id &&
+    search?.results.find((media: any) => media.id + "" === bigMatch.params.id);
+  console.log(clickedBox);
   return (
     <>
       {search && search.results.length > 0 ? (
-        <Row>
-          {search?.results.map((movie: any) => (
-            <Box
-              layoutId={movie.id + ""}
-              key={movie.id}
-              variants={boxVariants}
-              initial="normal"
-              whileHover="hover"
-              transition={{ type: "tween" }}
-              onClick={() => onBoxClicked(movie.id)}
-              bgphoto={makeImagePath(movie.backdrop_path || "", "w500")}
-            >
-              <Info variants={infoVariants}>
-                <h4>{movie.title ? movie.title : movie.name}</h4>
-              </Info>
-            </Box>
-          ))}
-        </Row>
+        <>
+          <Row>
+            {search?.results.map((movie: any) => (
+              <Box
+                layoutId={movie.id + ""}
+                key={movie.id}
+                variants={boxVariants}
+                initial="normal"
+                whileHover="hover"
+                transition={{ type: "tween" }}
+                onClick={() => onBoxClicked(movie.media_type, movie.id)}
+                bgphoto={makeImagePath(movie.backdrop_path || "", "w500")}
+              >
+                <Info variants={infoVariants}>
+                  <h4>{movie.title ? movie.title : movie.name}</h4>
+                </Info>
+              </Box>
+            ))}
+          </Row>
+          <AnimatePresence>
+            {bigMatch ? (
+              <>
+                <Overlay
+                  onClick={onOverlayClick}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                />
+                <BigMovie
+                  layoutId={bigMatch.params.id}
+                  style={{ top: scrollY.get() + 100 }}
+                >
+                  {clickedBox && (
+                    <>
+                      <BigCover
+                        style={{
+                          backgroundImage: `linear-gradient(to top, black, transparent), url(${makeImagePath(
+                            clickedBox.backdrop_path,
+                            "w500"
+                          )})`,
+                        }}
+                      />
+                      <BigTitle>
+                        {clickedBox.media_type === "tv"
+                          ? clickedBox.name
+                          : clickedBox.title}
+                      </BigTitle>
+                      <BigOverview>{clickedBox.overview}</BigOverview>
+                      <BigAverage>
+                        vote average: {clickedBox.vote_average}
+                      </BigAverage>
+                    </>
+                  )}
+                </BigMovie>
+              </>
+            ) : null}
+          </AnimatePresence>
+        </>
       ) : (
         <NoSearchData imgUrl={noSearchDataImage() || ""}>
           '{keyword}'검색 결과가 없습니다.
