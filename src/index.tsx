6d5bbb53162e31ec0e9784e9ad88cd4ba6e7e849
FE012 <twte96@gmail.com>
2023-06-07 15:44:11 +0900

    chore: 폰트 설정

--- src/index.tsx
+++ src/index.tsx
@@ -26,7 +26,7 @@ time, mark, audio, video {
   padding: 0;
   border: 0;
   font-size: 100%;
-  font: inherit;
+  font-family: 'Noto Sans', sans-serif;
   vertical-align: baseline;
 }
 /* HTML5 display-role reset for older browsers */
